455a628912dd2f1000d8a2a7dfe447d312eb0573
Legic <Legic2007@yandex.ru>
2023-12-24 18:44:28 +0300

    CRUD

--- src/main/java/ru/netology/controller/PostController.java
+++ src/main/java/ru/netology/controller/PostController.java
@@ -8,34 +8,62 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.Reader;
 
+
 public class PostController {
-  public static final String APPLICATION_JSON = "application/json";
-  private final PostService service;
+    public static final String APPLICATION_JSON = "application/json";
+    private final PostService service;
+    final Gson gson = new Gson();
 
-  public PostController(PostService service) {
-    this.service = service;
-  }
 
-  public void all(HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var data = service.all();
-    final var gson = new Gson();
-    response.getWriter().print(gson.toJson(data));
-  }
+    public PostController(PostService service) {
+        this.service = service;
+    }
 
-  public void getById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void all(HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var data = service.all();
+        response.getWriter().print(gson.toJson(data));
+    }
 
-  public void save(Reader body, HttpServletResponse response) throws IOException {
-    response.setContentType(APPLICATION_JSON);
-    final var gson = new Gson();
-    final var post = gson.fromJson(body, Post.class);
-    final var data = service.save(post);
-    response.getWriter().print(gson.toJson(data));
-  }
+    public void getById(long id, HttpServletResponse response) throws IOException {
+        // если элемент есть в репозитории, считываем и печатаем
+        if (service.getRepository().getItemsList()
+                .stream()
+                .anyMatch(c -> c.getId() == id - 1)) {
+            response.getWriter().print(service.getRepository().getItemsList().get((int) id - 1).toString());
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
 
-  public void removeById(long id, HttpServletResponse response) {
-    // TODO: deserialize request & serialize response
-  }
+    public void save(Reader body, HttpServletResponse response) throws IOException {
+        response.setContentType(APPLICATION_JSON);
+        final var post = gson.fromJson(body, Post.class);
+        final var data = service.save(post);
+
+        if (data.getId() == 0) {
+            service.getRepository().addPost(new Post(service.getRepository().getItemsCounter(), data.getContent()));
+            service.getRepository().iterateCounter();
+
+        } else if (service.getRepository().getItemsList()
+                .stream()
+                .anyMatch(c -> c.getId() == data.getId())) {
+            response.setStatus(HttpServletResponse.SC_CONFLICT);
+        }
+        response.getWriter().print(gson.toJson(data));
+    }
+
+    public void removeById(long id, HttpServletResponse response) throws IOException {
+        // если элемент есть в репозитории, то удаляем
+        if (service.getRepository().getItemsList()
+                .stream()
+                .anyMatch(c -> c.getId() == id - 1)) {
+            service.removeById(id);
+            response.getWriter().print("Пост " + id + " удален!");
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+
+    }
+
 }
--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -5,21 +5,50 @@ import ru.netology.model.Post;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 // Stub
 public class PostRepository {
-  public List<Post> all() {
-    return Collections.emptyList();
-  }
 
-  public Optional<Post> getById(long id) {
-    return Optional.empty();
-  }
+    private List<Post> itemsList;
+    private Long itemsCounter;
 
-  public Post save(Post post) {
-    return post;
-  }
+    public PostRepository() {
+        this.itemsList = new CopyOnWriteArrayList<>();
+        this.itemsCounter = 0L;
 
-  public void removeById(long id) {
-  }
+    }
+
+    public List<Post> all() {
+        return Collections.emptyList();
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.empty();
+    }
+
+    public Post save(Post post) {
+        return post;
+    }
+
+    public void removeById(long id) {
+        itemsList.remove((int) id - 1);
+    }
+
+    public List<Post> getItemsList() {
+        return itemsList;
+    }
+
+    public void addPost(Post post) {
+        itemsList.add(post);
+    }
+
+    public Long getItemsCounter() {
+        return itemsCounter;
+    }
+
+    public void iterateCounter() {
+        this.itemsCounter++;
+    }
+
 }
